a4a393c0c92c760aeae56c3525a25920e2fa9351
VidulaAdikari <it21127182@my.sliit.lk>
2022-09-18 23:21:43 +0530

    done

--- app/src/main/java/com/example/petpal/MainActivity.java
+++ app/src/main/java/com/example/petpal/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent1= new Intent(MainActivity.this,Profile.class);
+        Intent intent1= new Intent(MainActivity.this,AddGIG2.class);
         startActivity(intent1);
     }
 }
