4979c6b3996abdee6cd0ffebaca5811da2cc3a57
Sandaru-IT21001352 <it21001352@my.sliit.lk>
2022-10-28 20:09:41 +0530

    Pet edit and delete 100%

---
+++ app/src/main/java/com/example/petpal/ui/myPets/EditPetActivity.java
@@ -0,0 +1,16 @@
+package com.example.petpal.ui.myPets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.petpal.R;
+
+public class EditPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.petpal.R.layout.activity_edit_pet);
+    }
+}
